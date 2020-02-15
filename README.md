d95cdc37f1103720a9c3ddc4c757cfdcf436226e
Nathan Ramanathan <nathan6ramanathan@gmail.com>
2020-02-15 11:27:28 -0600

    applied gitignore and new readme

--- README.md
+++ README.md
@@ -1,17 +1,52 @@
+## Forked by Nathan^2
++ dims more smoothly and uses xprintidle for detecting idleness
++ systemd timer used to run at boot
++ Regarding the touch screen, the device disables it after dimming the screen.  Then, when the user taps on the screen, it brightens the screen and enables the touch screen inputs. This way, we avoid propagating the mouse inputs when we can't see the screen. **(which was an issue in the original repo)**
+
+## Running
+Simply type in the terminal of the project's directory:
+```
+make
+sudo ./timeout 30
+```
+where the 30 is 30 seconds of idleness
+
+## Systemd file setup for running script at boot:
+Enter into a terminal:
+```
+sudo systemctl edit --full backlight_dimmer.service
+```
+Enter the following into the new file:
+
+**Note: Change the ExecStart to your location of the timeout script**
+```
+[Unit]
+Description=Used to dim the raspberry pi's backlight upon idleness
+
+[Service]
+ExecStart=/home/pi/Documents/Github/myForks/backlight_dimmer/timeout 30
+
+[Install]
+WantedBy=multi-user.target
+
+```
+Then reboot!
+
+-----
 # Backlight dimmer
 Leaving the backlight on the Official Raspberry Pi touchscreen can quickly wear it out.
 If you have a use that requires the pi to be on all the time, but does not require the
 display on all the time, then turning off the backlight while not in use can dramatically
 increase the life of the backlight.
 
-Backlight dimmer will transparently dim the display backlight after there 
+Backlight dimmer will transparently dim the display backlight after there
 has been no input for a specifed timeout, independent of anything using the display
 at the moment. It will then turn the touchscreen back on when input is received. The
 timeout period is set by a command-line argument.
 
-**Note:** This does not stop the event from getting to whatever is running on the
+~**Note:** This does not stop the event from getting to whatever is running on the
 display. Whatever is running will still receive an event, even if the display
-is off.
+is off.~ Fixed in this Fork
 
 The program will use a linux event device like `/dev/input/event0` to receive events
 from the touchscreen, keyboard, mouse, etc., and `/sys/class/backlight/rpi-backlight/brightness`
@@ -36,20 +71,20 @@ sudo setuid  ./timeout
 Multiple devices may be specified.
 
 **Note:** It must be run as root or with `sudo` to be able to access the backlight. Or you can make the program setuid and owned by root
-It can be run at startup, for example by putting a line in 
+It can be run at startup, for example by putting a line in
 `/etc/rc.local`
 
 
 ### Conflict with console blanker
 
 When running this program without X Windows running, such as when running a Kivy
-program in the console at startup, you may run into a conflict with a console 
-blanker.  In such an instance, the backlight will be turned on, but with the 
+program in the console at startup, you may run into a conflict with a console
+blanker.  In such an instance, the backlight will be turned on, but with the
 console blanked, it seems like the backlight has not come on.
 
 In this case, follow one of these methods for disabling the console blanker:
-   * Raspbian Jessie : 
-     Add the following line to /etc/rc.local (on the line before the final exit 0) 
+   * Raspbian Jessie :
+     Add the following line to /etc/rc.local (on the line before the final exit 0)
      and reboot:
 ```
   sh -c "TERM=linux setterm -blank 0 >/dev/tty0"
@@ -58,7 +93,7 @@ In this case, follow one of these methods for disabling the console blanker:
    Even though /dev/tty0 is used, this should propagate across all terminals.
 
    * Raspbian Wheezy :
-     Edit /etc/kbd/config and change the values for the variable shown below, 
+     Edit /etc/kbd/config and change the values for the variable shown below,
      then reboot:
 ```
   BLANK_TIME=0
--- timeout.c
+++ timeout.c
@@ -1,19 +1,14 @@
 /* timeout.c - a little program to blank the RPi touchscreen and unblank it
    on touch.  Original by https://github.com/timothyhollabaugh
 
-   Loosely based on the original. The main difference is that the 
-   brightness is progressively reduced to zero.
+   Improvements listed in Readme.
 
-   On a touch event we reset the brightness to the original value (read when
-   the program started.
+   Improved and Forked by Nathan Ramanathan 02-08-2020+
+   First Written by Dougie Lawson 01-22-2019
 
-   Unless you change the permissions in udev this program needs to run as
-   root or setuid as root.
 
-   First Written by Dougie Lawson 01-22-2019 
-   Forked by Nathan Ramanathan 02-08-2020 +
    (C) Copyright 2019, Dougie Lawson, all right reserved.
-*/
+ */
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -29,33 +24,36 @@
 #include <signal.h>
 
 #define fade_amount 1
+#define TOUCH_SCREEN_INPUT_DEVICE 6 //found by typing `xinput --list` in terminal
+#define SLEEP_TIMEOUT_DURATION 1000 //in ms
 
 static FILE* brightfd;
-static uint32_t actual_brightness;
-static uint32_t max_brightness;
-static uint32_t current_brightness;
+static uint16_t actual_brightness;
+static uint16_t max_brightness;
+static uint16_t current_brightness;
 
 static void sig_handler(int _);
 static void set_screen_brightness(uint32_t brightness);
 static uint32_t fast_atoi( const char * str );
-static uint32_t GetIdleTime();
+static uint32_t get_idle_time();
+static void enable_touch_screen(bool enable);
 
 long int readint(char * filenm) {
-    FILE * filefd;
-    filefd = fopen(filenm, "r");
-    if (filefd == NULL) {
-        int err = errno;
-        printf("Error opening %s file: %d", filenm, err);
-        exit(1);
-    }
+		FILE * filefd;
+		filefd = fopen(filenm, "r");
+		if (filefd == NULL) {
+				int err = errno;
+				printf("Error opening %s file: %d", filenm, err);
+				exit(1);
+		}
 
-    char number[10];
-    char * end;
-    fscanf(filefd, "%s", & number);
-    printf("File: %s ,The number is: %s\n", filenm, number);
+		char number[10];
+		char * end;
+		fscanf(filefd, "%s", (char *) &number);
+		printf("File: %s ,The number is: %s\n", filenm, number);
 
-    fclose(filefd);
-    return strtol(number, & end, 10);
+		fclose(filefd);
+		return strtol(number, &end, 10);
 }
 ///////////////////////////
 void sleep_ms(int milliseconds);
@@ -69,124 +67,143 @@ void sleep_ms(int milliseconds);
 // void sleep_ms(int milliseconds);
 void sleep_ms(int milliseconds) // cross-platform sleep function
 {
-    #ifdef WIN32
-    Sleep(milliseconds);
-    #elif _POSIX_C_SOURCE >= 199309L
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
-    #else
-    usleep(milliseconds * 1000);
-    #endif
+	#ifdef WIN32
+		Sleep(milliseconds);
+	#elif _POSIX_C_SOURCE >= 199309L
+		struct timespec ts;
+		ts.tv_sec = milliseconds / 1000;
+		ts.tv_nsec = (milliseconds % 1000) * 1000000;
+		nanosleep(&ts, NULL);
+	#else
+		usleep(milliseconds * 1000);
+	#endif
 }
 ////////////////////////////
 
 static void sig_handler(int _)
 {
-   (void)_;	
-   set_screen_brightness(max_brightness);
-   exit(0);
+		(void)_;
+		set_screen_brightness(max_brightness);
+		exit(0);
 }
 
 static void set_screen_brightness(uint32_t brightness){
-    fprintf(brightfd, "%d\n", brightness);
-    fflush(brightfd);
-    fseek(brightfd, 0, SEEK_SET);
+		fprintf(brightfd, "%d\n", brightness);
+		fflush(brightfd);
+		fseek(brightfd, 0, SEEK_SET);
 }
 
 static uint32_t fast_atoi( const char * str )
 {
-    uint32_t val = 0;
-    while( *str ) {
-        val = val*10 + (*str++ - '0');
-    }
-    return val;
+		uint32_t val = 0;
+		while( *str ) {
+				val = val*10 + (*str++ - '0');
+		}
+		return val;
 }
 
-static uint32_t GetIdleTime() {
-        uint32_t idle_time;
-        FILE *fp;
-	char path[1035];
-	
-	/* Open the command for reading. */
-	fp = popen("sudo -u pi env DISPLAY=:0 xprintidle", "r");
-	if (fp == NULL) {
-	  printf("Failed to run command\n" );
-	  exit(1);
-	}
-	
-	/* Read the output a line at a time - output it. */
-	while (fgets(path, sizeof(path), fp) != NULL) {
-          idle_time = fast_atoi(path);
-          if((int)idle_time<0)idle_time=0;
-	}
-	/* close */
-        pclose(fp);
-        return idle_time;
+static uint32_t get_idle_time() {
+		uint32_t idle_time;
+		FILE *fp;
+		char path[1035];
+
+		/* Open the command for reading. */
+		fp = popen("sudo -u pi env DISPLAY=:0 xprintidle", "r");
+		if (fp == NULL) {
+				printf("Failed to run command\n" );
+				exit(1);
+		}
+
+		/* Read the output a line at a time - output it. */
+		while (fgets(path, sizeof(path), fp) != NULL) {
+				idle_time = fast_atoi(path);
+				if((int)idle_time<0) idle_time=0;
+		}
+		/* close */
+		pclose(fp);
+		return idle_time;
+}
+
+static void enable_touch_screen(bool enable) {
+		FILE *fp;
+		char final_command[70];
+		char set_command[] = "%s %s %d";
+		char disable_enable[10];
+		strcpy(disable_enable, (enable) ? "--enable" : "--disable");
+		char command[] = "sudo -u pi env DISPLAY=:0 xinput";
+		sprintf(final_command, set_command, command, disable_enable, TOUCH_SCREEN_INPUT_DEVICE);
+		printf("%s\n", final_command);
+		/* Open the command for reading. */
+		fp = popen((char *) final_command, "r");
+		if (fp == NULL) {
+				printf("Failed to run command\n" );
+				exit(1);
+		}
+		/* close */
+		pclose(fp);
 }
 
 int main(int argc, char * argv[]) {
-    printf("Hello World\n");
-    signal(SIGINT, sig_handler);
-    if (argc < 2) {
-        printf("Usage: timeout <timeout_sec>\n");
-        exit(1);
-    }
-    int i;
-    int tlen;
-    int timeout;
-    tlen = strlen(argv[1]);
-    for (i = 0; i < tlen; i++)
-        if (!isdigit(argv[1][i])) {
-            printf("Entered timeout value is not a number\n");
-            exit(1);
-        }
-    timeout = atoi(argv[1]);
+		// printf("Hello World\n");
+		signal(SIGINT, sig_handler);
+		if (argc < 2) {
+				printf("Usage: timeout <timeout_sec>\n");
+				exit(1);
+		}
+		uint16_t tlen;
+		uint16_t timeout;
+		tlen = strlen(argv[1]);
+		for (uint16_t i = 0; i < tlen; i++)
+				if (!isdigit(argv[1][i])) {
+						printf("Entered timeout value is not a number\n");
+						exit(1);
+				}
+		timeout = atoi(argv[1]);
 
-    char actual[53] = "/sys/class/backlight/rpi_backlight/actual_brightness";
-    char max[50] = "/sys/class/backlight/rpi_backlight/max_brightness";
-    char bright[46] = "/sys/class/backlight/rpi_backlight/brightness";
+		// char actual[53] = "/sys/class/backlight/rpi_backlight/actual_brightness";
+		char max[50] = "/sys/class/backlight/rpi_backlight/max_brightness";
+		char bright[46] = "/sys/class/backlight/rpi_backlight/brightness";
 
-    brightfd = fopen(bright, "w");
-    if (brightfd == NULL) {
-        int err = errno;
-        printf("Error opening %s file: %d", bright, err);
-        exit(1);
-    }
+		brightfd = fopen(bright, "w");
+		if (brightfd == NULL) {
+				int err = errno;
+				printf("Error opening %s file: %d", bright, err);
+				exit(1);
+		}
 
-    max_brightness = readint(max);
-    current_brightness = max_brightness;
-    actual_brightness = max_brightness; //readint(actual);
+		max_brightness = readint(max);
+		current_brightness = max_brightness;
+		actual_brightness = max_brightness; //readint(actual);
 
-    set_screen_brightness(max_brightness);
+		set_screen_brightness(max_brightness);
 
-    printf("actual_brightness %d, max_brightness %d\n", actual_brightness, max_brightness);
+		printf("actual_brightness %d, max_brightness %d\n", actual_brightness, max_brightness);
 
-    bool fade_direction = true;
-    while (1) {
-        if (fade_direction && GetIdleTime() < timeout*1E4) {
-            for (i = 0; i <= max_brightness; i++) {
-                current_brightness += fade_amount;
-                if (current_brightness > max_brightness) current_brightness = max_brightness;
-                //printf("Brightness now %d\n", current_brightness);
-                set_screen_brightness(current_brightness);
-                sleep_ms(1);
-            }
-            fade_direction = false;
-        } else if (!fade_direction && GetIdleTime() >= timeout*1E4) {
-            if (current_brightness > 0) {
-                for (i = max_brightness; i > 0; i--) {
-                    current_brightness -= fade_amount;
-                    if (current_brightness < 0) current_brightness = 0;
-                    //printf("Brightness now %d\n", current_brightness);
-                    set_screen_brightness(current_brightness);
-                    sleep_ms(2);
-                }
-            }
-            fade_direction = true;
-        }
-        //printf("Idle Time: %d\n", GetIdleTime());
-        sleep_ms(1000);
-    }
+		bool fade_direction = true;
+		while (1) {
+				if (fade_direction && get_idle_time() < timeout*1E4) {
+						for (uint16_t i = 0; i <= max_brightness; i++) {
+								current_brightness += fade_amount;
+								if (current_brightness > max_brightness) current_brightness = max_brightness;
+								//printf("Brightness now %d\n", current_brightness);
+								set_screen_brightness(current_brightness);
+								sleep_ms(1);
+						}
+						fade_direction = false;
+				} else if (!fade_direction && get_idle_time() >= timeout*1E4) {
+						if (current_brightness > 0) {
+								for (uint16_t i = max_brightness; i > 0; i--) {
+										current_brightness -= fade_amount;
+										if (current_brightness < 0) current_brightness = 0;
+										//printf("Brightness now %d\n", current_brightness);
+										set_screen_brightness(current_brightness);
+										sleep_ms(2);
+								}
+								enable_touch_screen(false);
+						}
+						fade_direction = true;
+				}
+				//printf("Idle Time: %d\n", get_idle_time());
+				sleep_ms(SLEEP_TIMEOUT_DURATION);
+		}
 }
